8f1da801924c3afdf999dfbc7e34e116d6186161
BilalSsiddique <bsiddiqui@students.uit.edu>
2023-06-16 18:03:25 +0500

    updarted

--- app.js
+++ app.js
@@ -37,13 +37,6 @@ app.use("*", (req, res) => {
   res.status(404).json({ error: "route Not found" });
 });
 
-app.use(express.static(path.join(__dirname, `${build}`)));
-
-app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, `${build}`, "index.html")
-  );
-});
 
 app.get("/todos", (req, res) => {
   const token =
