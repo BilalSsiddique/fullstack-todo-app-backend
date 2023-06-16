02de68d1982834070b005d1f5a454b3df60fe95d
BilalSsiddique <bsiddiqui@students.uit.edu>
2023-06-16 17:38:34 +0500

    updarted

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.use("*", (req, res) => {
   res.status(404).json({ error: "route Not found" });
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "public/")));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "public/", "index.html"));
 });
 
 app.get("/todos", (req, res) => {
